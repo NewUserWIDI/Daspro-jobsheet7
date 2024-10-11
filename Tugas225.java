a38c322d4f3f8a68e1d9970fb3fe1ba014e61052
NewUserWIDI <wwidi4087@gmail.com>
2024-10-11 10:04:46 +0700

    SiakadFor25

--- Tugas225.java
+++ Tugas225.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Tugas224 {
+public class Tugas225 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
 
@@ -29,6 +29,6 @@ public class Tugas224 {
         System.out.println("Total pembayaran: Rp " + total);
         sc.close();
     }
-} {
+} 
     
-}
+
